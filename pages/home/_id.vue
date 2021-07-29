dc7e5af9a3d30fee6a8ac0024b97f942d65d5473
Lenildo Luan <lenildoluan@gmail.com>
2021-07-28 23:33:55 -0300

    Module 1 and 2

---
+++ pages/home/_id.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div style="display: flex;">
+            <img v-for="image in home.images" :key="image" :src="image" width="200px" height="150px">
+        </div>
+        {{home.title}} <br>
+        ${{ home.pricePerNight }} / night <br>
+        <img src="/images/marker.svg" width="20px" height="20px"> {{ home.location.address }} {{ home.location.city }} {{ home.location.state }} {{ home.location.country }} <br/>
+        <img src="/images/star.svg" width="20px" height="20px"> {{ home.reviewValue }} <br>
+        {{ home.guests }} guests, {{ home.bedrooms }} rooms, {{ home.beds }} beds, {{home.bathroom}} bath <br>
+    </div>
+</template>
+
+<script>
+import homes from '~/data/homes'
+export default {
+    layout: 'red',
+    head() {
+        return {
+            title: this.home.title
+        }
+    },
+    data() {
+        return {
+            home: {}
+        }
+    },
+    created() {
+        const home = homes.find((home) => home.objectID == this.$route.params.id)
+        this.home = home
+    }
+}
+</script>
